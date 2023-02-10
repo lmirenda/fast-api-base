ef689f7147fb4babf3446148a8828762072dcb6d
Jhoan Rodriguez <jhoan@simplenight.com>
2023-02-10 08:59:59 -0500

    changed type id table hero from int to uuid

--- src/models/hero.py
+++ src/models/hero.py
@@ -1,10 +1,16 @@
 from typing import Optional
 
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from sqlmodel import Field, SQLModel
+import uuid as uuid_pkg
 
 
 class Hero(SQLModel, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
+    id: Optional[uuid_pkg.UUID] = Field(
+        default_factory=uuid_pkg.uuid4,
+        primary_key=True,
+        index=True,
+        nullable=False,
+    )
     name: str
     secret_name: str
     age: Optional[int] = None
