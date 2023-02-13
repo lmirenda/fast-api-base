ad1c3f5ef6aa73896ca3f3c055a41e61d858d1e0
Jhoan Rodriguez <jhoan@simplenight.com>
2023-02-13 13:55:07 -0500

    documented SQLALCHEMY_DATABASE_URI validator

--- src/core/config.py
+++ src/core/config.py
@@ -19,6 +19,17 @@ class Settings(BaseSettings):
 
     @validator("SQLALCHEMY_DATABASE_URI", pre=True)
     def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
+        """
+        The assemble_db_connection function is a Pydantic validator that constructs and
+        returns the PostgreSQL database connection string as a PostgresDsn object
+        if the connection string is not already present.
+        It takes three arguments: cls, which is the class where the validator is defined;
+        v, which is the input value to validate; and values, which is a dictionary of previously validated values.
+        If v is a string, it returns it as is. Otherwise, it uses the PostgresDsn.build() function
+        to construct the connection string from the POSTGRES_USER, POSTGRES_PASSWORD,
+        DATABASE_HOSTNAME, POSTGRES_PORT, and POSTGRES_DB values found in the values dictionary.
+        Finally, it returns the resulting connection string as a PostgresDsn object.
+        """
         if isinstance(v, str):
             return v
         return PostgresDsn.build(
