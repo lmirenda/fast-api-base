15f85f26b84b4fe38496e473bc644c98adb55c7c
Jose Alvarez <jose@simplenight.com>
2023-02-14 15:27:06 -0500

    Removed comments.

--- src/models/sn_base_model.py
+++ src/models/sn_base_model.py
@@ -4,15 +4,11 @@ from datetime import datetime
 
 class SNBaseModel(BaseModel):
     class Config:
-        # enable JSON encoding/decoding
         json_encoders = {
             datetime: lambda v: v.strftime("%Y-%m-%dT%H:%M:%S"),
         }
-        # Whether to strip leading and trailing whitespace for str & byte types
         anystr_strip_whitespace = True
-        # support classes that are not pydantic models
         arbitrary_types_allowed = True
-        # make all underscore attrs are private
         underscore_attrs_are_private = True
 
     def dict(self, *args, **kwargs):
