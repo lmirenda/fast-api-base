b22fd18cfbc59e0dffae26498b274cd4c4c9f2de
lmirenda <lucamateomirenda@gmail.com>
2023-02-14 16:50:22 -0300

    Add helper models for example model Hero

--- src/models/hero.py
+++ src/models/hero.py
@@ -4,13 +4,31 @@ from sqlmodel import Field, SQLModel
 import uuid as uuid_pkg
 
 
-class Hero(SQLModel, table=True):
+class HeroBase(SQLModel):
+    name: str = Field(index=True)
+    secret_name: str
+    age: Optional[int] = Field(default=None, index=True)
+
+
+class Hero(HeroBase, table=True):
     id: Optional[uuid_pkg.UUID] = Field(
         default_factory=uuid_pkg.uuid4,
         primary_key=True,
         index=True,
         nullable=False,
     )
-    name: str
-    secret_name: str
+
+
+class HeroCreate(HeroBase):
+    pass
+
+
+class HeroRead(HeroBase):
+    id: int
+
+
+class HeroUpdate(SQLModel):
+    name: Optional[str] = None
+    secret_name: Optional[str] = None
     age: Optional[int] = None
+
