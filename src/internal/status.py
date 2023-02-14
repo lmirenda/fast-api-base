5dc1a55f78bc98ba95ee2c5ef86c444c52a568f6
Jhoan Rodriguez <jhoan@simplenight.com>
2023-02-14 10:23:50 -0500

    Applied ApiRouters

---
+++ src/internal/status.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter
+from ..core.config import settings
+
+router = APIRouter(
+    prefix="/admin",
+    tags=["admin"],
+)
+
+
+@router.get("/info")
+async def info():
+    return {"app_name": settings.APP_NAME}
+
+
+@router.get("/database")
+async def database():
+    return {"database": "OK"}
--- src/main.py
+++ src/main.py
@@ -1,15 +1,22 @@
 from fastapi import FastAPI
-from .core.config import APP_CONFIGS, settings
+from starlette.middleware.cors import CORSMiddleware
 
+from .core.config import APP_CONFIGS, settings
+from .routers import items
+from .internal import status
 
 app = FastAPI(**APP_CONFIGS)
 
+# Set all CORS origins enabled
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
-@app.get("/")
-async def main_route():
-    return {"message": "Hey, It is me Goku"}
-
-
-@app.get("/info")
-async def info():
-    return {"app_name": settings.APP_NAME}
+# Add Routers
+app.include_router(items.router, prefix=settings.API_V1_STR)
+app.include_router(status.router, prefix=settings.API_V1_STR)
