258f2e4536ce3546224d5a2d661beae22ccc8da4
lmirenda <lucamateomirenda@gmail.com>
2023-02-14 17:28:30 -0300

    Add docstrings for test configuration fixtures

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -11,6 +11,10 @@ from src.main import app, get_session
 
 @pytest.fixture(name="session")
 def session_fixture():
+    """
+    Fixture that creates an instance of the SQLAlchemy session.
+    All tests are run on a flushed database instance.
+    """
     engine = create_engine(
         "sqlite://", connect_args={"check_same_thread": False}, poolclass=StaticPool
     )
@@ -21,6 +25,7 @@ def session_fixture():
 
 @pytest.fixture(name="client")
 def client_fixture(session: Session):
+    """Creates a new synchronous FastAPI test client that connects to the session test database."""
     def get_session_override():
         return session
 
@@ -33,6 +38,7 @@ def client_fixture(session: Session):
 
 @pytest.fixture(name="async_client")
 async def async_client_fixture(session: Session) -> AsyncIterator[AsyncClient]:
+    """Creates a new asynchronous FastAPI test client that connects to the session test database."""
     def get_session_override():
         return session
 
