598ad6826755f0fecc6128de14b34d9b7d54873d
lmirenda <lucamateomirenda@gmail.com>
2023-02-16 10:26:35 -0300

    Refactor get session method to db session directory

--- src/db/session.py
+++ src/db/session.py
@@ -4,3 +4,11 @@ from ..core.config import settings
 
 engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def get_session():
+    try:
+        session = SessionLocal()
+        yield session
+    finally:
+        session.close()
